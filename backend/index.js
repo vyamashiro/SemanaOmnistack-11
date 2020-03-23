75e331275ccdcb31c878e2eb771c08e277c13880
Victor Katsuo Yamashiro <vkatsuo@gmai.com>
2020-03-23 16:37:43 -0300

    first day

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Victor Yamashiro'
+    });
+});
+
+console.log("servidor rodando");
+
+app.listen(3333);
