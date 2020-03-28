9ef4cf3c135a41cc8795481c0085b6ab60725013
vyamashiro <victor.yamashiro@gmail.com>
2020-03-28 19:56:33 -0300

    dia3 pagina de register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
-import './styles.css'
+import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
@@ -18,10 +19,10 @@ export default function Logon () {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
